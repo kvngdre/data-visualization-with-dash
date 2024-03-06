344098782abf39209f56e97229308c5eb73e400d
kvngdre <kennedyugo2@gmail.com>
2024-03-06 13:36:52 +0100

    extracted the menu component to it's own module

--- src/app.py
+++ src/app.py
@@ -2,13 +2,12 @@
 from dash import Dash, dcc, html
 
 from components import (average_avocado_price_component,
-                        avocados_sold_component, header_component)
+                        avocados_sold_component, header_component,
+                        menu_component)
 from data import fetch_data
 
 # Read data
 data = fetch_data()
-regions = data["region"].sort_values().unique()
-avocado_types = data["type"].sort_values().unique()
 
 #Initialize the app
 external_stylesheets = [
@@ -27,59 +26,7 @@ app.title = "Avocado Analytics: Understand Your Avocados!"
 app.layout = html.Div(
     children = [
         header_component .component,
-        html.Div(
-            children=[
-                html.Div(
-                    children=[
-                        html.Div(children="Region", className="menu-title"),
-                        dcc.Dropdown(
-                            id="region-filter",
-                            options=[
-                                {"label": region, "value": region}
-                                for region in regions
-                            ],
-                            value="Albany",
-                            clearable=False,
-                            className="dropdown",
-                        ),
-                    ]
-                ),
-                html.Div(
-                    children=[
-                        html.Div(children="Type", className="menu-title"),
-                        dcc.Dropdown(
-                            id="type-filter",
-                            options=[
-                                {
-                                    "label": avocado_type.title(),
-                                    "value": avocado_type,
-                                }
-                                for avocado_type in avocado_types
-                            ],
-                            value="organic",
-                            clearable=False,
-                            searchable=False,
-                            className="dropdown",
-                        ),
-                    ],
-                ),
-                html.Div(
-                    children=[
-                        html.Div(
-                            children="Date Range", className="menu-title"
-                        ),
-                        dcc.DatePickerRange(
-                            id="date-range",
-                            min_date_allowed=data["Date"].min().date(),
-                            max_date_allowed=data["Date"].max().date(),
-                            start_date=data["Date"].min().date(),
-                            end_date=data["Date"].max().date(),
-                        ),
-                    ]
-                ),
-            ],
-            className="menu",
-        ),
+        menu_component.component,
         html.Div(
             children=[
                 average_avocado_price_component.component,
